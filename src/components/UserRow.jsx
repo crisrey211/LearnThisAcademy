f1dcc0a0e657634fd264295950afcef0b936e017
christian_reynaltt <creynaltt@maristak.net>
2022-12-20 23:49:12 +0100

    UseState

--- src/components/UserRow.jsx
+++ src/components/UserRow.jsx
@@ -1,19 +1,30 @@
 import UserRole from "./UserRole"
 import style from "./UserRow.module.css"
 import UserStatus from "./UserStatus"
-const UserRow = (props) => {
-    const { name, active, role, ...restProps } = props
+import React from "react"
 
+
+const UserRow = ({ name, active: active, role, ...restProps }) => {
+
+    const [activeState, setActiveState] = React.useState(active)
+
+    console.log(active);
 
     return < div className={style.wrapper} {...restProps}>
         <div className={style.name}>
             <span>{name}</span>
         </div>
         <div className={style.status}>
-            <UserStatus active={active} />
+            <UserStatus active={activeState} />
         </div >
         <div className={style.role}>
             <UserRole role={role} />
+        </div>
+        <div className={style.action}>
+            <button onClick={() => {
+                setActiveState(!activeState)
+            }}
+            >{activeState ? 'Desactivar' : 'Activar'}</button>
         </div>
     </div >
 }
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,7 +2,11 @@ import UserRow from "./UserRow";
 import style from "./UsersList.module.css"
 
 const UsersList = ({ children, user }) => {
-    const usersRendered = user.map(usuario => <UserRow key={usuario.name} {...usuario} />);
+    const usersRendered = user.length > 0 ? (
+        user.map(usuario => <UserRow key={usuario.name} {...usuario} />)
+    ) : <p>No hay usuario</p>
+
+
     return (< div className={style.wrapper} >
         {children}
         {usersRendered}
