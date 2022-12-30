2ec308c91568217c3d969a155f7b0b7a8c74b042
christian_reynaltt <creynaltt@maristak.net>
2022-12-30 02:06:09 +0100

    Formulario No controlado

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import UserRow from "./UserRow";
 import style from "./UsersList.module.css"
 
 const UsersList = ({ children, user }) => {
-    const usersRendered = user.length > 0 ? (
-        user.map(usuario => <UserRow key={usuario.name} {...usuario} />)
+
+    const [search, setSearch] = useState('')
+
+    const usersFiltered = search
+        ? user.filter(item => item.name.toLowerCase().startsWith(search.toLocaleLowerCase()))
+        : user
+
+    const usersRendered = usersFiltered.length > 0 ? (
+        usersFiltered.map(usuario => <UserRow key={usuario.name} {...usuario} />)
     ) : <p>No hay usuario</p>
 
 
     return (< div className={style.wrapper} >
         {children}
+        <form onSubmit={ev => {
+            ev.preventDefault();
+            setSearch(ev.target.search.value)
+        }}>
+            <input type='search' name="search"></input>
+            <button type="submit">Buscar</button>
+        </form>
         {usersRendered}
     </div >)
 }
